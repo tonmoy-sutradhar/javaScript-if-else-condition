cdd81eb03ffa444efcd7fb5f6bbd0e78e9f5a5df
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-08-30 15:32:53 +0600

    Calculate if else condition done.

--- resturantCal.js
+++ resturantCal.js
@@ -1,5 +1,5 @@
 const price = 1200;
-const age = 60;
+const age = 43;
 
 if (age <= 12) {
   console.log("You can eat for free.");
@@ -9,4 +9,18 @@ if (age <= 12) {
   const payment = price - discount;
   console.log("Your discount amount: " + discount);
   console.log("Your total payment: " + payment);
+} else if (age >= 50) {
+  // 30% discount......
+  const discount = (price * 20) / 100;
+  const payment = price - discount;
+  console.log("Your discount amount: " + discount);
+  console.log("Your total payment: " + payment);
+} else if (age >= 40) {
+  // 30% discount......
+  const discount = (price * 10) / 100;
+  const payment = price - discount;
+  console.log("Your discount amount: " + discount);
+  console.log("Your total payment: " + payment);
+} else {
+  console.log("Total payment: " + price);
 }
