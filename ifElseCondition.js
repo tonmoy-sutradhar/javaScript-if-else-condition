2becc4fe0472e6473dcbcc0ba084f1a89e833c77
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-08-30 12:36:00 +0600

    File added.

---
+++ ifElseCondition.js
@@ -0,0 +1,20 @@
+/*
+*Tui jodi amake khawas
+*taile ami jabo
+
+---------------------------------
+*If you buy food for me
+    i will come
+
+---------------------------------
+if briyani price less than 300 taka
+    i will buy
+    
+---------------------------------
+*********STRUCTURE***************
+
+if (condition)
+{
+  context-------;
+}
+*/
